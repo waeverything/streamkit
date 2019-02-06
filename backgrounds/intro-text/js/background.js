ca46e51c1b60a94f4851da2bd9778c64a763c756
Fabian <44451855+FaberoM@users.noreply.github.com>
2019-02-06 18:34:12 +0200

    Merge intro scenes to one

--- backgrounds/intro-text/js/background.js
+++
@@ -1,9 +0,0 @@
-let amount = 20;
-
-for (let i = 0; i < amount; i++) {
-  let elem = document.createElement("img");
-  elem.setAttribute("src", "svg/3dbar.svg");
-  elem.style.animationDelay = Math.floor(Math.random() * -60) + "s"
-  elem.style.opacity = Math.random() * 1
-  document.getElementById("objects").appendChild(elem);
-}
