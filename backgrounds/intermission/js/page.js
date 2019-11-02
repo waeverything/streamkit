b3790bcf98c1fc661a80f19518c77a319cdc30ba
Fabian Mäkilä <44451855+FaberoM@users.noreply.github.com>
2019-11-03 00:27:03 +0200

    Make it easier to set text.

---
+++ backgrounds/intermission/js/page.js
@@ -0,0 +1,39 @@
+//Background
+let amount = 20;
+
+for (let i = 0; i < amount; i++) {
+  let elem = document.createElement("img");
+  elem.setAttribute("src", "svg/3dbar.svg");
+  elem.style.animationDelay = Math.floor(Math.random() * -60) + "s"
+  elem.style.opacity = Math.random() * 1
+  document.getElementById("objects").appendChild(elem);
+}
+
+//Typewriter
+let typingSpeed = 150;
+
+let url_string = window.location.href;
+let url = new URL(url_string);
+let txt = url.searchParams.get("s");
+
+let i = 0;
+
+window.setInterval(function(){
+  i = 0;
+  typeWriter();
+}, 20000);
+
+function typeWriter() {
+  if (txt != null) {
+    if(i < txt.length) {
+      document.getElementById("typewriter").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, typingSpeed);
+    }
+    setTimeout(function() {
+      document.getElementById("typewriter").innerHTML = "";
+    }, 15000);
+  }
+}
+
+window.onload = typeWriter();
